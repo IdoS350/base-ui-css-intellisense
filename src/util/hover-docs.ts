3369d800a7a6b7afbf41b9dd403e5c40d0e0db61
Ido S <idoshmuly@gmail.com>
2026-04-25 15:41:57 +0300

    organized hover info

--- src/util/hover-docs.ts
+++ src/util/hover-docs.ts
@@ -4,9 +4,16 @@ const BASE_UI_GITHUB = 'https://github.com/mui/base-ui/blob/master'
 
 export function buildHoverDocs(entry: AttributeIndex): string {
   const { attribute, components, sourceFile } = entry
-  let md = ''
+  let md = `### Base UI: \`${attribute.name}\`\n\n`
   if (attribute.description) md += `${attribute.description}\n\n`
-  md += `**Used by:** ${components.join(', ')}\n\n`
+
+  if (attribute.values?.length) {
+    md += `**Values:** ${attribute.values.map(({ value }) => `\`${value}\``).join(', ')}\n\n`
+  }
+
+  md += '\n#### Used by\n'
+  md += `${components.join(', ')}\n\n`
+
   if (sourceFile) {
     md += `[View source on GitHub](${BASE_UI_GITHUB}/${sourceFile})`
   }
